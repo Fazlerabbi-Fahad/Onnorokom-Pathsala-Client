dc82ecfa7cd4a853a085104cb2342b13938f7795
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-10-25 23:52:09 +0600

    first commit

---
+++ src/pages/Categories/Courses/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Courses.css';
+
+const Courses = () => {
+    return (
+        <div>
+            This is Courses
+        </div>
+    );
+};
+
+export default Courses;
